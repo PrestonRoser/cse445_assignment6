cf0ba1092019bc932d9cdb7a354cd70b77ec4bc8
Deliver_me_pizza <69709146+Rated-F-Freeware@users.noreply.github.com>
2026-04-28 17:49:05 -0700

    adding raw hash code
    adding raw hash code just in case TA needs to see the code

---
+++ passwordhash-raw/hash.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace hashpassword
+{
+    public class hash
+    {
+        public static string createpwdhash(string password)
+        {
+            //checking to make sure password isnt empty
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new Exception("Password cannot be empty");
+            }
+            //converting string to byte array 
+            byte[] passwordbyte = Encoding.UTF8.GetBytes(password);
+            //creating an instance of sha then computing hash of password
+            var sha256 = SHA256.Create();
+            byte[] hash = sha256.ComputeHash(passwordbyte);
+
+            //converting to base64 string for storage could use hex 
+            return Convert.ToBase64String(hash);
+            
+        }
+        public static bool hashcompare(string password, string hash)
+        {
+            //generating hash of entered password and comparing them to one another
+            string entered_password = createpwdhash(password);
+            return entered_password == hash;
+        }
+    }
+}
