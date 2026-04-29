8cc1caa63473232d7dbe1950440586eff460e8e9
PrestonRoser <prestonroser@gmail.com>
2026-04-28 22:30:51 -0700

    fix: role verification through Page_Load

--- ProtectedMember/MemberPage.aspx.cs
+++ ProtectedMember/MemberPage.aspx.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Security;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using WebApplication1_Assignment5;
 
 namespace WebApplication1_Assignment5
 {
@@ -13,49 +8,10 @@ namespace WebApplication1_Assignment5
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                //RefreshList();
-            }
+            if (Global.GetSessionRole(Session) != "Member")
+                Response.Redirect("~/Login.aspx");
 
-            user_lb.Text = "Welcome " + "Member!";
-
-
-            //Session_start();
-
-        }
-        protected void selected_criminal(object sender, EventArgs e)
-        {
-
-           /* string select_criminal = Criminaldb_lb.SelectedValue;
-            List<Criminal_Scum> criminals = CriminalDataBase.GetCriminals(Session);
-            Criminal_Scum criminal_record = criminals.Find(criminal => criminal.criminal_name == select_criminal);*/
-
-            /*if (criminal_record != null)
-            {
-                Criminalinfo_label.Text = "Criminal Name: " + criminal_record.criminal_name + "<br/>" +
-                    " Crime commited: " + criminal_record.crime + "<br/>" +
-                    " Original state: " + criminal_record.crime_location + "<br/>" +
-                    " Time booked: " + criminal_record.processed_time.ToString("MM/dd/yyyy hh:mm tt");
-            }
-*/
-        }
-
-
-        public void Session_start()
-        {
-            //checking is cookie session is null
-            if (Session["UserType"] == null || Session["UserType"].ToString() != "Member")
-            {
-                //Might need to change this to the correct name if the page is different
-                Response.Redirect("~/Default.aspx");
-            }
-            
-        }
-        public void RefreshList()
-        {
-            //Criminaldb_lb.DataSource = CriminalDataBase.GetCriminals(Session);
-            //Criminaldb_lb.DataBind();
+            user_lb.Text = "Welcome Member!";
         }
 
         protected void logout_btn_Click(object sender, EventArgs e)
@@ -67,7 +23,7 @@ namespace WebApplication1_Assignment5
 
         protected void criminal_btn_Click(object sender, EventArgs e)
         {
-              Response.Redirect("~/Criminal_database.aspx");
+            Response.Redirect("~/Criminal_database.aspx");
         }
     }
 }
